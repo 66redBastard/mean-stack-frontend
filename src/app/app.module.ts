20e072e77eb2120d637ed414faa708da2e87439a
66redBastard <66redbastard@gmail.com>
2021-05-10 06:57:48 +0300

    alert service added, user and alert interface added. app, home, alert updated, bootstrap 5 added, fake backend added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AlertComponent } from './_components/alert/alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
